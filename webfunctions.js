056659275400b74120d60956d017344d81d79818
samuelg900 <47197769+samuelg900@users.noreply.github.com>
2025-05-15 02:04:27 -0400

    Created webfunctions.js

---
+++ webfunctions.js
@@ -0,0 +1,14 @@
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > lastScrollY) {
+    // Scrolling down
+    document.getElementById('navbar').classList.add('nav_disappear'); 
+    document.getElementById('navbar').classList.remove('nav_appear'); 
+  } else {
+    // Scrolling up
+    document.getElementById('navbar').classList.remove('nav_disappear');
+    document.getElementById('navbar').classList.add('nav_appear'); 
+  }
+  lastScrollY = window.scrollY;
+});
