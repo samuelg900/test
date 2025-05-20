dea8a3fbed7fd25e6badb4f0b445370f39238dd7
samuelg900 <47197769+samuelg900@users.noreply.github.com>
2025-05-20 01:29:03 -0400

    Update webfunctions.js

--- webfunctions.js
+++ webfunctions.js
@@ -1,14 +1,16 @@
 let lastScrollY = window.scrollY;
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > lastScrollY) {
-    // Scrolling down
-    document.getElementById('navbar').classList.add('nav_disappear'); 
-    document.getElementById('navbar').classList.remove('nav_appear'); 
-  } else {
-    // Scrolling up
-    document.getElementById('navbar').classList.remove('nav_disappear');
-    document.getElementById('navbar').classList.add('nav_appear'); 
+  if (window.scrollY > 300) {
+    if (window.scrollY > lastScrollY) {
+      // Scrolling down
+      document.getElementById('navbar').classList.add('nav_disappear');
+      document.getElementById('navbar').classList.remove('nav_appear');
+    } else {
+      // Scrolling up
+      document.getElementById('navbar').classList.remove('nav_disappear');
+      document.getElementById('navbar').classList.add('nav_appear');
+    }
+    lastScrollY = window.scrollY;
   }
-  lastScrollY = window.scrollY;
 });
