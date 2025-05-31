c0d6a207d7b7d0f99425280b09a1fe95259baf36
Luis <samuelg900.sm@gmail.com>
2025-05-31 00:22:16 -0400

    updated more files

--- webfunctions.js
+++ webfunctions.js
@@ -2,15 +2,17 @@ let lastScrollY = window.scrollY;
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 300) {
-    if (window.scrollY > lastScrollY) {
-      // Scrolling down
-      document.getElementById('navbar').classList.add('nav_disappear');
-      document.getElementById('navbar').classList.remove('nav_appear');
-    } else {
-      // Scrolling up
-      document.getElementById('navbar').classList.remove('nav_disappear');
-      document.getElementById('navbar').classList.add('nav_appear');
+    if (lastScrollY + 20 < window.scrollY || lastScrollY - 20 > window.scrollY) {
+      if (window.scrollY > lastScrollY) {
+        // Scrolling down
+        document.getElementById('navbar').classList.add('nav_disappear');
+        document.getElementById('navbar').classList.remove('nav_appear');
+      } else {
+        // Scrolling up
+        document.getElementById('navbar').classList.remove('nav_disappear');
+        document.getElementById('navbar').classList.add('nav_appear');
+      }
+      lastScrollY = window.scrollY;
     }
-    lastScrollY = window.scrollY;
   }
 });
